4803b0f62302240e6b593e83e6e06e7fc7ad5ac8
Steve Unixson <unixson@gmail.com>
2019-09-25 00:00:16 +0300

    fixed ESLINT

--- src/classes/List.class.ts
+++ src/classes/List.class.ts
@@ -7,7 +7,6 @@ import cheerio from 'cheerio';
 import ScraperClass from './Scraper.class';
 
 export default class ListClass extends ScraperClass {
-
   public $ = cheerio.load(`${this.get()}`);
 
   public async follow(href: string): Promise<void> {}
